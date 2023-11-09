612d800539aaf197e3e62c3aecbe195f9c3e8ad5
aryan3839 <92433348+aryan3839@users.noreply.github.com>
2023-11-09 14:15:37 +0530

    Update weather.service.ts

--- myweatherapp/src/app/weather.service.ts
+++ myweatherapp/src/app/weather.service.ts
@@ -9,6 +9,6 @@ export class WeatherService {
   constructor(private http: HttpClient) { }
 
   getweather(city: string, units: string) {
-    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&{your_Open_API_Key}&units=' + units);
+    return this.http.get('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&{344928ea2ac3c77290d7dc680978abd2}&units=' + units);
   }
 }
